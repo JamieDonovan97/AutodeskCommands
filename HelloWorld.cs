b5a13268f441f35f83463d569612488cd7be4b00
JamieDonovan97 <jamie.donovan97@gmail.com>
2024-09-25 21:15:00 +1000

    Small Update

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -7,15 +7,24 @@ using System.Reflection.Metadata;
 
 namespace AutodeskCommands
 {
+    /// <summary>
+    /// A simple C# Plugin for AutoCAD that displays "HelloWorld" to the command line
+    /// </summary>
     public class HelloWorld
     {
+        /// <summary>
+        /// The Hello command that displays "Hello World!" in the command line.
+        /// </summary>
         [CommandMethod("HelloWorld")]
         public void Hello()
         {
+            // Get the active document
             var document = Application.DocumentManager.MdiActiveDocument;
             
+            // Get the editor object
             var editor = document.Editor;
             
+            // Display "Hello World!" in the command line
             editor.WriteMessage("Hello World!");
         }
     }
