97444af4f222c860f4488ef3d2e3e675ca8ab7d3
JamieDonovan97 <jamie.donovan97@gmail.com>
2024-09-26 08:06:57 +1000

    Refactor
    Extract helper methods for active document and editor retrieval

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -1,32 +1,66 @@
+using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.Runtime;
-using Autodesk.AutoCAD.ApplicationServices.Core;
-using System;
-using System.Linq;
-using System.Reflection.Metadata;
+using Autodesk.AutoCAD.EditorInput;
+using Application = Autodesk.AutoCAD.ApplicationServices.Core.Application;
 
-
-namespace AutodeskCommands
-{
+namespace AutodeskCommands;
     /// <summary>
     /// A simple C# Plugin for AutoCAD that displays "HelloWorld" to the command line
+    /// based on the "Create Your First AutoCAD Plugin" by Autodesk University
     /// </summary>
-    public class HelloWorld
+public class Commands : IExtensionApplication
+{
+    /// <summary>
+    /// Initializes the plugin.
+    /// Runs on AutoCAD Startup
+    /// </summary>
+    public void Initialize()
     {
-        /// <summary>
-        /// The Hello command that displays "Hello World!" in the command line.
-        /// </summary>
-        [CommandMethod("HelloWorld")]
-        public void Hello()
-        {
-            // Get the active document
-            var document = Application.DocumentManager.MdiActiveDocument;
+        var editor = GetEditor();
+        editor.WriteMessage("\nPlugin Loaded Successfully.");
+    }
+    
+    /// <summary>
+    /// Terminates the plugin.
+    /// Unload Resources
+    /// </summary>
+    public void Terminate()
+    {
+        Application.DocumentManager.MdiActiveDocument.Editor.WriteMessage("\nPlugin terminated.");
+    }
+    
+    /// <summary>
+    /// The Hello command that displays "Hello World!" in the command line.
+    /// </summary>
+    [CommandMethod("HelloWorld")]
+    public void HelloWorld()
+    {
+        // Get the active document
+        var document = Application.DocumentManager.MdiActiveDocument;
             
-            // Get the editor object
-            var editor = document.Editor;
+        // Get the editor object
+        var editor = document.Editor;
             
-            // Display "Hello World!" in the command line
-            editor.WriteMessage("Hello World!");
-        }
+        // Display "Hello World!" in the command line
+        editor.WriteMessage("\nHello World!");
+    }
+
+    /// <summary>
+    /// Helper method to get the active AutoCAD document.
+    /// </summary>
+    /// <returns> Returns the active AutoCAD document </returns>
+    private Document GetActiveDocument()
+    {
+        return Application.DocumentManager.MdiActiveDocument;
+    }
+    
+    /// <summary>
+    /// Helper method to get the active AutoCAD document.
+    /// </summary>
+    /// <returns> Returns the active AutoCAD document </returns>
+    private Editor GetEditor()
+    {
+        return GetActiveDocument().Editor;
     }
 }
 
